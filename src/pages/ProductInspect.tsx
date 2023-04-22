bac8158cc3a0fb5c924fa7073f2fafde90ebe161
RadoslavB3 <radoslav.bena1@gmail.com>
2023-04-22 17:46:10 +0200

    merge

--- src/pages/ProductInspect.tsx
+++ src/pages/ProductInspect.tsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import { useNavigate, useParams } from '@tanstack/react-router';
-import { Suspense, useEffect, useState } from 'react';
-import { getDoc, onSnapshot } from 'firebase/firestore';
-import { Box, Typography } from '@mui/material';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-import {
-	Furniture,
-	furnitureDocument,
-	furnituresCollection,
-	reviewsDocument
-} from '../firebase';
-import Product3DView from '../components/ProductPreview';
-=======
 import { useParams } from '@tanstack/react-router';
 import { useEffect, useState } from 'react';
 import { DocumentSnapshot, onSnapshot } from 'firebase/firestore';
@@ -21,7 +5,6 @@ import { Box } from '@mui/material';
 
 import { Furniture, furnitureDocument } from '../firebase';
 import ProductInspectDetail from '../components/ProductInspectDetail';
->>>>>>> main
 
 const ProductInspect = () => {
 	const params = useParams();
