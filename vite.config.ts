f0e17aa276c6b263b6e976bfa576c391eccb7e13
Gilderko <martin.sepela007@gmail.com>
2023-05-10 14:39:40 +0200

    feat: Added meta tags and PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()]
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['tableIcon.svg', 'webSitePreview.png'],
+			manifest: {
+				short_name: 'Furni 3D',
+				name: 'Furni 3D',
+				description:
+					'The best furniture shop with 3D previews of the furniture :)',
+				icons: [
+					{
+						src: '/grid.svg',
+						type: 'image/svg+xml',
+						sizes: '48x48 192x192 512x512'
+					}
+				],
+				theme_color: '#f2d45c',
+				background_color: '#ffffff'
+			}
+		})
+	]
 });
